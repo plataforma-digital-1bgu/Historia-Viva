eef607dde9af73b365ba533082aca04bd8a5efe7
Lissette Torres <lissetttorres380@gmail.com>
2026-02-13 18:14:21 -0500

    Se agrega el Proyecto

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+<script>
+// ===== NAVEGACIÓN BLOQUES =====
+function irBloque1() {
+    window.location.href = "Bloques/bloque1.html";
+}
+
+function irBloque2() {
+    window.location.href = "Bloques/bloque2.html";
+}
+
+function irBloque3() {
+    window.location.href = "Bloques/bloque3.html";
+}
+
+function irBloque4() {
+    window.location.href = "Bloques/bloque4.html";
+}
+
+function irBloque5() {
+    window.location.href = "Bloques/bloque5.html";
+}
+
+</script>
